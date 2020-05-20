fa057420a709c8aa885b0f6d0b01ccb930d9f490
Yash Kanodia <yashkanodia16@gmail.com>
2020-05-20 18:54:29 -0400

    Exercise 7 Solution

--- Exercise 7/script.js
+++ Exercise 7/script.js
@@ -11,28 +11,38 @@ questionArray.push(new Question('What\'s the professor name?',['Mark','John','Ka
 questionArray.push(new Question('Which course are you enrolled in?',['Html5','CSS','JavaScript'],2));
 questionArray.push(new Question('Did you like the course?',['Yes','No'],0));
 
-var quesNo = [Math.floor(Math.random() * questionArray.length)];
 
 Question.prototype.random = function(){
-    return this.question;
+    console.log(this.question);
+    for(var x=0;x<this.answers.length;x++){
+        console.log(x+':'+" "+this.answers[x]);
+    }
+    return prompt('which is the correct option?');
 }
 
-var randomElem = questionArray[quesNo].random();
-console.log(randomElem);
-
-for(var x=0;x<questionArray[quesNo].answers.length;x++){
-    console.log(x+':'+" "+questionArray[quesNo].answers[x]);
-}
-
-var ans = prompt('which is the correct option?');
-
-Question.prototype.isCorrect = function(ans){
-    if(ans == this.correct)
+Question.prototype.isCorrect = function(randomElem){
+    if(randomElem == this.correct){
         console.log('Correct Answer!');
+        count++;
+    }        
     else
-        console.log('Try Again!');  
+        console.log('Try Again!'); 
+    console.log('Total points:' + count); 
+    
 }
 
-questionArray[quesNo].isCorrect(ans);
+var count =0;
+
+ function infi(){
+    var quesNo = [Math.floor(Math.random() * questionArray.length)];
+    var randomElem = questionArray[quesNo].random();
+    console.log(randomElem);
+    if(randomElem != 'Exit' && randomElem != 'exit'){    
+        questionArray[quesNo].isCorrect(randomElem);
+        infi();
+    }
+}
+infi();
+
 
 })();
